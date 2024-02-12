c6288b07191a88b838049aca1540919a99e8da23
devtya1 <98102495+devtya1@users.noreply.github.com>
2024-02-12 19:59:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,11 +17,22 @@ function change(){
 var i = 0;
 var txt = "Hii, See TBH i dont have much to say but i'll just say a few things. I know that am not perfect nor am that caring but i promise abh se i'll pakka improve myself and just wanna say, YOU ARE THE BEST GF IN THIS WHOLE WORLD WHO CARES ALOT FOR ME, I DON'T HAVE WORDS TO DEFINE YOU BUT YOU ARE THE BEST! and i'll never ever leave you, i promise and i'll never ever make my baccha let cry cause of me. Sometimes we'll even face many hard times but i promise you that whatever the condition is i'll never leave my baccha alone in this world... I Love You soo Much!";
 var speed = 50;
+var time;
 
 function typeWriter() {
+    document.getElementById("nxt").style.display = "none";
   if (i < txt.length) {
     document.getElementById("para").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
+  launch();
+}
+
+function launch(){
+    time = setTimeout(open, 35000);
+}
+
+function open(){
+    document.getElementById("nxt").style.display = "block";
 }
